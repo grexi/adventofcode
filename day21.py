d905ae7f67acb66d64f63c7ca3322b1dc5f23028
Gregor Dorfbauer <gregor.dorfbauer@gmail.com>
2016-12-21 20:56:21 +0100

    non-brute force version with proof

--- day21.py
+++ day21.py
@@ -24,8 +24,11 @@ def move(start, x, y):
 
 def scramble(start, instr):
 	start = [s for s in start]
+	intermediate = []
 
 	for ins in instr:
+		intermediate.append(["".join(start), ins])
+		#print("%r %r" % (start, ins))
 		ins = ins.strip().split()
 		if ins[0] == "swap":
 			if ins[1] == "position":
@@ -65,20 +68,22 @@ def scramble(start, instr):
 			y = int(ins[5])
 			start = move(start, x, y)
 
-	return "".join(start)
+	return "".join(start), intermediate
 
 def reverse_scramble_brute(start, instr):
 	for inputs in permutations("abcdefgh"):
-		if scramble(inputs, instr) == start:
-			return inputs
+		if scramble(inputs, instr)[0] == start:
+			return "".join(inputs)
 	return False
 
 def reverse_scramble(start, instr):
 	start = [s for s in start]
+	intermediate = []
 
-	for ins in reversed(instr):
-		#print("%r %r" % (start, ins))
-		ins = ins.strip().split()
+	for inss in reversed(instr):
+
+		#print("%r %r" % (start, inss))
+		ins = inss.strip().split()
 		if ins[0] == "swap":
 			if ins[1] == "position":
 				x = int(ins[2])
@@ -103,11 +108,12 @@ def reverse_scramble(start, instr):
 			elif ins[1] == "based":
 				letter = ins[6]
 				lpos = start.index(letter)
-				steps = 1 + lpos
-				if lpos >= 4:
-					steps += 1
-				steps = steps % len(start)
-				start = rotate(start, steps)
+				inv = { 1: 1, 2: -2, 3: -6, 6: 0, 7: 4, 0: 1, 5: -5, 4: -1}
+				#steps = lpos - 1
+				#if lpos >= 4:
+				#	steps -= 1
+				#steps = steps % len(start)
+				start = rotate(start, inv[lpos])
 		elif ins[0] == "reverse":
 			x = int(ins[2])
 			y = int(ins[4])
@@ -116,8 +122,8 @@ def reverse_scramble(start, instr):
 			x = int(ins[2])
 			y = int(ins[5])
 			start = move(start, y, x)
-
-	return "".join(start)
+		intermediate.append(["".join(start), inss])
+	return "".join(start), intermediate
 
 instrs = [
 "swap position 4 with position 0",
@@ -130,12 +136,27 @@ instrs = [
 "rotate based on position of letter d"
 ]
 
-print(scramble("abcde", instrs))
+#print(scramble("abcde", instrs))
+#print(reverse_scramble("decab", instrs))
 
-
+#sys.exit()
 f = open("day21/input.txt", "r")
 instructions = f.readlines()
-print(scramble("abcdefgh", instructions))
-print(reverse_scramble("fbgdceah", instructions))
-print(reverse_scramble_brute("fbgdceah", instructions))
+res, im1 = scramble("abcdefgh", instructions)
+print(res)
+res2, im2 = reverse_scramble("fbgdceah", instructions)
+im1.reverse()
+print res2
+for i in range(len(im2)):
+	print("%r %r %r" % (im1[i][0] == im2[i][0], im1[i], im2[i]))
+
+if False:
+	for inputs in permutations("abcdefgh"):
+		if reverse_scramble(scramble(inputs, instructions)[0], instructions)[0] != "".join(inputs):
+			print("%r does not work" % ("".join(inputs)))
+
+
+
+#print(reverse_scramble("fbgdceah", instructions)[0])
+#print(reverse_scramble_brute("fbgdceah", instructions))
 
